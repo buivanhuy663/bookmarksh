2d35d6044506b7ed97e5720f6fcfccc09e025737
huy2.bui <huy2.bui@lge.com>
2024-10-01 15:51:14 +0700

    [15:51:09](01/10/2024) ok

--- generate-package-json.js
+++ generate-package-json.js
@@ -62,7 +62,7 @@ const customPackageJson = {
 
 customPackageJson.name = "bookmarksh";
 customPackageJson.displayName = "Bookmarks Tree";
-customPackageJson.version = "0.0.14";
+customPackageJson.version = "0.0.16";
 
 fs.writeFileSync(customPackageJsonPath, JSON.stringify(customPackageJson, null, 2));
 
