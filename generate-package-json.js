2a0d0722b1da35a57079c017a5941b644ff96814
buivanhuy663 <buivanhuy.663@gmail.com>
2024-10-05 16:39:25 +0700

    [16:39:23](05/10/2024) ok

--- generate-package-json.js
+++ generate-package-json.js
@@ -126,7 +126,7 @@ const customPackageJson = {
 
 customPackageJson.name = "bookmarksh";
 customPackageJson.displayName = "Bookmarks Tree";
-customPackageJson.version = "0.1.0";
+customPackageJson.version = "0.1.1";
 
 fs.writeFileSync(customPackageJsonPath, JSON.stringify(customPackageJson, null, 2));
 
