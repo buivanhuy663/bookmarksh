5bf1e052fc01aae385a974f9459597fa070d5319
buivanhuy.663 <buivanhuy.663@gmail.com>
2024-10-30 13:51:08 +0700

    [13:51:08](30/10/2024) hot fix

--- generate-package-json.js
+++ generate-package-json.js
@@ -127,7 +127,7 @@ const customPackageJson = {
 
 customPackageJson.name = "bookmarksh";
 customPackageJson.displayName = "Bookmarks Tree";
-customPackageJson.version = "0.1.2";
+customPackageJson.version = "0.1.3";
 
 fs.writeFileSync(customPackageJsonPath, JSON.stringify(customPackageJson, null, 2));
 
