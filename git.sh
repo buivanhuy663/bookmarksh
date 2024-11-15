f549adfa404c39e3e77b5d822d570da92f97f1d5
buivanhuy.663 <buivanhuy.663@gmail.com>
2024-11-15 16:37:28 +0700

    [16:37:28](15/11/2024)

---
+++ git.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$1" == "commit" ]; then
+
+currentTime=`date +'[%H:%M:%S](%d/%m/%Y)'`
+
+echo "$currentTime $1"
+
+cd src/bookmark-provider
+git add .
+git commit -a -m "$currentTime $2"
+if [ "$3" == "p" ]; then
+    git push
+	echo "Pushing to remote"
+fi
+
+cd -
+git add .
+git commit -a -m "$currentTime $3"
+if [ "$3" == "p" ]; then
+    git push
+	echo "Pushing to remote"
+fi
+
+fi
+
+
+if [ "$1" == "pull" ]; then
+cd src/bookmark-provider
+git pull
+
+cd -
+git pull
+fi
+
